a33d3d316d8db2d69801d68701e32a90b814c78f
Change Agent <keasiley@icloud.com>
2023-02-18 20:54:07 -0600

    Q&A JS added to assests folder and to HTML

---
+++ Assests/ScriptQ&A.js
@@ -0,0 +1,31 @@
+
+
+
+const questions = [
+    {
+        title: 'Commonly used data types DO NOT include:',
+        choices: ['strings', 'boolean', 'alerts', 'numbers'],
+        answer: 'booleans'
+    },
+    {
+        title: 'The condition in an if/else statement is enclosed with ___________ .',
+        choices: ['commas', 'curly brackets', 'quotes', 'parentheses'],
+        answer: 'parentheses'
+    },
+    {
+        title: 'Arrays in JavaSrcipt can be used to store ________ .',
+        choices: ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
+        answer: 'all of the above'
+    },
+    {
+        title: 'String values must be enclosed within _______ when assisging variables.',
+        choices: ['commas', 'curly brackets', 'quotes', 'parentheses'],
+        answer: 'quotes'
+    },
+    {
+        title: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+        choices: ['JavaScript', 'terminial bash', 'for loops', 'console.log'],
+        answer: 'console.log'
+
+    },
+];
